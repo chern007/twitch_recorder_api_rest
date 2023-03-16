434d53a1e43b96f135f9a3962e47eda2685764b4
chern007 <carloshernandezcrespo@gmail.com>
2023-03-16 22:12:30 +0100

    ok

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+import sys
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return jsonify({'message': 'Hello World'})
 
-@app.route('/hello/<name>')
-def hello(name):
-    return jsonify({'message': f'Hello {name}'})
+@app.route("/")
+def hello():
+    version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+    message = "Hello World from Flask in a uWSGI Nginx Docker container with Python {} (default)".format(
+        version
+    )
+    return message
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=80)
